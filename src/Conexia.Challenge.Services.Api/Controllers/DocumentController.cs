2abeb1748a7f16e513fe1fc094c386d54a86c887
Caio Canalli <ccanalli@fh.com.br>
2020-01-25 19:07:50 -0300

    Fix problems with route in document and add configurations to Startup

--- src/Conexia.Challenge.Services.Api/Controllers/DocumentController.cs
+++ src/Conexia.Challenge.Services.Api/Controllers/DocumentController.cs
@@ -1,16 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Conexia.Challenge.Application.Documents.Requests;
+using Conexia.Challenge.Application.Documents.Responses;
+using Conexia.Challenge.Application.Documents.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace Conexia.Challenge.Services.Api.Controllers
 {
-    public class DocumentController : Controller
+    [ApiController]
+    [Route("api/document")]
+    public class DocumentController : ControllerBase
     {
-        public IActionResult Index()
+        readonly IDocumentAppService _documentAppService;
+
+        public DocumentController(IDocumentAppService documentAppService)
         {
-            return View();
+            _documentAppService = documentAppService;
+        }
+
+        [HttpPost]
+        [Route("upload")]
+        public async Task<IActionResult> Upload([FromBody]UploadRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            await _documentAppService.UploadAsync(request);
+
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("filter")]
+        public async Task<IActionResult> Filter([FromQuery]FilterRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            FilterResponse response =
+                await _documentAppService.FilterAsync(request);
+
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody]UpdateRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            await _documentAppService.UpdateAsync(request);
+
+            return Ok();
         }
     }
 }
